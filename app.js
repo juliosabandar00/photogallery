665ac01bf78201ed7e087ada2badaba7b16aec88
Julio Sabandar <kjuliosabandar@gmail.comgit commit test.htmlgit push -u origin mastergit statusgit config --global user.email kjuliosabandar@gmail.com>
2020-03-27 07:43:31 +0700

    adjust to heroku standard

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3700;
+const port = process.env.PORT || 4000;
 const session = require('express-session')
 const indexRouter = require('./routes/index.js')
 const authRouter = require('./routes/authRouter');
